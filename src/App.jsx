f48f23e06dbfd0745f222ef368d18260e322affd
Fatai Ayeloja <fayeloja@gmail.com>
2025-10-07 19:15:46 +0100

    Removed duplicateed Recipedetail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Routes, Route, Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
-import RecipeDetail from "./pages/RecipeDetail";
 import Products from "./pages/Products";
 import OurStory from "./pages/OurStory";
 import Contact from "./pages/Contact";
-import ReviewsCarousel from "./components/ReviewsCarousel";
+import { lazy, Suspense } from "react";
+const ReviewsCarousel = lazy(() => import("./components/ReviewsCarousel"));
 import ScrollToTop from "./components/ScrollToTop";
 import ProductDetails from "./pages/ProductDetails";
 import RecipeDetails from "./pages/RecipeDetails";
@@ -15,25 +15,26 @@ import Recipes from "./pages/Recipes";
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      <NavBar />
-      {/* ScrollToTop resets scroll position on route change.
-          In SPAs, navigation does not trigger a full page reload,
-          so scroll position can persist between pages. This component
-          ensures users always start at the top of a new page, improving UX. */}
+      {/* Ensures scroll resets on route change for better UX */}
       <ScrollToTop />
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         {/* Dynamic product route */}
         <Route path="/products/:slug" element={<ProductDetails />} />
-
-        <Route path="/ourstory" element={<OurStory />} />
-        <Route path="/contact" element={<Contact />} />
         {/* Recipes routes */}
         <Route path="/recipes" element={<Recipes />} />
         <Route path="/recipes/:slug" element={<RecipeDetails />} />
+        <Route path="/ourstory" element={<OurStory />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-      <ReviewsCarousel />
+      {/* Render ReviewsCarousel only on the home page */}
+      <Suspense fallback={<div>Loading reviews...</div>}>
+        <Routes>
+          <Route path="/" element={<ReviewsCarousel />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
   );
