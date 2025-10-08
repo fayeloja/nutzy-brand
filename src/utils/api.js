44b9f58a63d45f30636b5394c72b4bdad1a6fd35
Fatai Ayeloja <fayeloja@gmail.com>
2025-10-08 18:40:30 +0100

    configure api and frontend config for server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,8 @@
 // Example: src/utils/api.js
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const sendContactForm = async (data) => {
-  const res = await fetch("http://localhost:5000/api/contact", {
+  const res = await fetch(`${API_BASE_URL}/api/contact`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
